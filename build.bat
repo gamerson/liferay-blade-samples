7abe53941cb40a4f48dab3d60c5465746996f778
Lawrence Lee <lawrence.lee@liferay.com>
2016-10-17 16:43:36 -0500

    Pass in proxy and mirrors

--- build.bat
+++ build.bat
@@ -13,6 +13,6 @@ call cd maven
 call mvn --fail-at-end clean package
 call cd %repoDir%
 call cd liferay-workspace
-call gradlew.bat clean build -Pliferay.workspace.bundle.url=http://mirrors/releases.liferay.com/portal/7.0.2-ga3/liferay-ce-portal-tomcat-7.0-ga3-20160804222206210.zip -Phttp.proxyHost=squid.lax.liferay.com -Phttp.proxyPort=3128
+call gradlew.bat clean build
 call cd %repoDir%
 call gradlew.bat outputFilesTest diff
