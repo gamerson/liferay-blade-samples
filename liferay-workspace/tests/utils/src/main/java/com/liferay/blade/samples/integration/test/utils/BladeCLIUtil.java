c25b7dae0b81a0b362f4d6c6b9a59ea05ec29f6c
Lawrence Lee <lawrence.lee@liferay.com>
2017-08-02 12:24:51 -0500

    improve error filtering logic

--- liferay-workspace/tests/utils/src/main/java/com/liferay/blade/samples/integration/test/utils/BladeCLIUtil.java
+++ liferay-workspace/tests/utils/src/main/java/com/liferay/blade/samples/integration/test/utils/BladeCLIUtil.java
@@ -14,8 +14,6 @@
 
 package com.liferay.blade.samples.integration.test.utils;
 
-import static org.junit.Assert.assertTrue;
-
 import aQute.bnd.osgi.Domain;
 import aQute.bnd.version.Version;
 
@@ -32,6 +30,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+
+import org.junit.Assert;
 
 /**
  * @author Lawrence Lee
@@ -99,16 +99,21 @@ public class BladeCLIUtil {
 		for (String string : errorList) {
 			string = string.trim();
 
-			if (!string.isEmpty() &&
-				(!string.contains("Picked up JAVA_TOOL_OPTIONS:")) ||
-				(!string.startsWith("sh: warning: setlocale: LC_ALL: " +
-					"cannot change locale (en_US.UTF-8)"))) {
+			if (!string.contains("Picked up JAVA_TOOL_OPTIONS:") || 
+				!string.contains("sh: warning: setlocale")) {
 
 				filteredErrorList.add(string);
 			}
 		}
 
-		assertTrue(filteredErrorList.toString(), filteredErrorList.isEmpty());
+		Assert.assertTrue(
+			filteredErrorList.toString(), filteredErrorList.size() <= 1);
+
+		if (filteredErrorList.size() == 1) {
+			Assert.assertTrue(
+				filteredErrorList.toString(),
+				filteredErrorList.get(0).isEmpty());
+		}
 
 		output = StringUtil.toLowerCase(output);
 
